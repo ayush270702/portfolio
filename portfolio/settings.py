54d5bfd1e655a7910b94e855a0a66d81404ead19
Ayush <ayushshende02@gmail.com>
2023-05-16 14:55:16 +0530

    d

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR + "/static/"
+# STATIC_ROOT = BASE_DIR / "static"
 
 
 
